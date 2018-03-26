a3bd77a6cfe377f51ad38da6ee48c9ea6803f759
Shihab <shihabdinajpur@gmail.com>
2018-03-26 21:57:43 +0600

    Update Find-Projects

--- wp-content/themes/find-consultant-child/archive-project.php
+++ wp-content/themes/find-consultant-child/archive-project.php
@@ -2,20 +2,20 @@
 /**
  * The template for displaying project listings
  *
- * @package HireBee\Templates
+ * @package Find-Consultant\Templates
  * @since 1.0.0
  */
 ?>
-<div class="container">
-<div id="primary" class="content-area row">
+<div class="container find-prjt">
+    <div id="primary" class="content-area row">
 
-	<div id="main" class="large-8 columns">
+        <div id="main" class="large-8 columns">
 
-		<?php get_template_part( 'parts/loop', HRB_PROJECTS_PTYPE ); ?>
+            <?php get_template_part( 'parts/loop', HRB_PROJECTS_PTYPE ); ?>
 
-	</div><!-- #main -->
+        </div><!-- #main -->
 
-	<?php get_sidebar( 'archive' ); ?>
+        <?php get_sidebar( 'archive' ); ?>
 
-</div><!-- #primary -->
+    </div><!-- #primary -->
 </div>
